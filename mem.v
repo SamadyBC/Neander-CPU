5fbc3d920b1d97f7962039314cbe0e1bea642f7c
SamadyBC <samadybiasoli@gmail.com>
2026-09-03 21:00:05 -0300

    feat: Improving ram file

--- mem.v
+++ mem.v
@@ -3,13 +3,17 @@ module MEM(
         input wire [7:0] rem_d, mux1,
         input wire r_z, write, read, clr, mux_sel1, rem_e, rdm_e,
         input wire clk,
-        output reg [7:0] rdm_out
+        output wire [7:0] rdm_out
     );
 
-    reg current_state, next_state;
+    //Conexoes entre blocos 
+    wire [7:0] w_mem_data;
+
+    //Registradores de Estado da Maquina
+    reg [1:0] current_state, next_state;
 
     //Registradores de Endereço e de Dados da Memoria
-    reg [7:0] rem, rdm;
+    reg [7:0] rem, rdm, mux_output;
 
 
     parameter wait_m = 2'b00,
@@ -19,37 +23,46 @@ module MEM(
 
     //Logica Resgistrador de Estados
     always @ ( posedge clk or posedge r_z) begin
-        if( r_z == 1'b0) begin
-            current_state = next_state;
-        end else if (r_z == 1'b1) begin
-            current_state = wait_m;
-            //zera os registradores de endereco e dados aqui? Ou no always abaixo
+        if(r_z) begin
+            current_state <= wait_m;
+        end else begin
+            current_state <= next_state;
         end
     end
 
     //Logica dos Registradores de dados, enderecos e memoria
     always @ ( posedge clk  or posedge r_z) begin
-        if (rem_e) begin
-            rem = rem_d;
-        end
-        case(current_state)
-            read_m: begin     
-                if (read && rdm_e) begin //mux sel entra aqui? NAO PRECISA mais da flag "read" aqui ne?
-                    //rdm = mem[rem];
+        if (r_z) begin
+            rdm <= 8'h00;
+            rem <= 8'h00;
+        end else begin
+            if (rdm_e) begin 
+                rdm <= mux_output;
+            end
+            if (rem_e) begin
+                rem <= rem_d;
+            end
+            case(current_state)
+                wait_m: begin
+                    //Nao faz nada
                 end
-            end
-            write_m: begin
-                //mem[rem] = rdm;
-            end
-            clear_m: begin
-                //mem = 0;
-            end
+                read_m: begin     
+                    //w_mem_data = mem[rem]
+                end
+                write_m: begin
+                    //mem[rem] = rdm;
+                end
+                clear_m: begin
+                    //mem = 0; - Ou nao limpar ou reset de todas as posicoes usando loop
+                end
 
-        endcase
+            endcase
+        end
     end
 
     // Logica dos Proximos Estados
-    always @ (write or read or clr or mux_sel1 or rem_e or rdm_e)begin //avaliar sinais necessarios nessa lista de sensibilidade, clk nao eh necessario mas e o "mux_sel1"?
+    always @ (*)begin
+        next_state = current_state;
         case(current_state)
             wait_m:  begin
                 if (write == 1) begin
@@ -70,5 +83,17 @@ module MEM(
                 next_state = wait_m;
         endcase
     end
+
+    //Logica Combinacional Independente do Clock
+    always @ (*) begin
+        case(mux_sel1)
+            1'b0: mux_output = w_mem_data;
+            1'b1: mux_output = mux1;
+            default: mux_output = 8'h00;
+        endcase
+    end
+
+    assign rdm_out = rdm;
+
 
 endmodule
