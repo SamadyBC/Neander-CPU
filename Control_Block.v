123809d00c3e46a7035b3f48bdab3503af110d1f
Rodrigo Lopes <rodrigomattosdf@gmail.com>
2026-09-04 09:49:32 -0300

    Novas Arquiteturas

---
+++ Control_Block.v
@@ -0,0 +1,11 @@
+module Control_Block (
+    input wire [7:0] x, y,
+    input wire [2:0] op_alu,
+    output reg [7:0] out,
+    output wire n, z
+);
+
+
+
+
+endmodule
---
+++ Temporization_architecture.v
@@ -0,0 +1,10 @@
+module Temporization_architecture (
+    input wire clk,
+    input wire halt, goto_t0
+    output reg  t0, t1, t2, t3, t4, t5, t6, t7
+);
+
+
+
+
+endmodule
